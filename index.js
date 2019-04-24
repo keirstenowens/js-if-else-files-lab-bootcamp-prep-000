9bf582f9fb32f9ab40a557a5d69978c32044a646
Keirsten Owens <keirstenowens@gmail.com>
2019-04-24 16:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ if (name === "Joe") {
 } else {
   console.log("Expected the name to be Susan")
 }
+if (height === 74) {
+  console.log("The height is correct")
+} else {
+  console.log("Expected the height to be 70")
+}
 // Don't worry about this
 module.exports = { name, height
 }
